fc201e79547beac600f9af201c0f3677d51a9e7a
Rian Priambodo <rian@localhost.localdomain>
2022-01-03 21:21:18 +0700

    Add basic table

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="flex items-center justify-between flex-wrap bg-red-600 p-4 shadow-md">
+        <nav className="flex items-center justify-between flex-wrap bg-gradient-to-b from-green-500 to-green-600 p-4 shadow-md">
             <div className="flex items-center flex-no-shrink text-white mr-6">
                 <Link to="/">
                     <span className="text-xl tracking-tight">
